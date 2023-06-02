630c8017b4abdc625be1ce67b7df1957add60e6a
BJDonnovan <mihailshulak079@gmail.com>
2023-06-03 01:39:13 +0700

    Final commit

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { set_time } from "./script2";
-import { calculate } from "./script2";
-import './styles.css'
-set_time();
-let sendButton = form.solve;
-sendButton.addEventListener("click", calculate);
